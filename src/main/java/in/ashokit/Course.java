185c232557c84699dc6eea33a39ed22962d6a6df
rajani78devi <rajani78devi@gmail.com>
2023-03-28 19:56:48 +0530

    commited

---
+++ src/main/java/in/ashokit/Course.java
@@ -0,0 +1,43 @@
+package in.ashokit;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "COURSE_DTLS")
+public class Course {
+
+	@Id
+	private Integer cid;
+	private String name;
+	private Double price;
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+}
